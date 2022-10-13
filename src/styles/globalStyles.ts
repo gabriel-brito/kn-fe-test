4a302c574cc45443872530a2093e1caf1c39d8e0
Gabriel Santos <mfkrgabriel@gmail.com>
2022-10-13 19:35:26 -0300

    Add global styles

---
+++ src/styles/globalStyles.ts
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from 'styled-components'
+
+const theme = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html, body {
+    height: 100%;
+  }
+
+  body {
+    font-family: Roboto, -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+`
+
+export default theme
