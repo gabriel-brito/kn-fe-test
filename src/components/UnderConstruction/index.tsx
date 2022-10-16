c0cc779ca9315b3c35ea8c92281911ff7de23535
Gabriel Santos <mfkrgabriel@gmail.com>
2022-10-16 16:52:03 -0300

    Add under construction modal

---
+++ src/components/UnderConstruction/index.tsx
@@ -0,0 +1,9 @@
+import * as S from './styles'
+
+export default function UnderConstruction() {
+  return (
+    <S.Wrapper>
+      <S.Title>Unfortunately, there's nothing here.</S.Title>
+    </S.Wrapper>
+  )
+}
---
+++ src/components/UnderConstruction/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.section`
+  background-color: var(--white);
+  border-radius: 4px;
+  box-shadow: 0px 1px 2px 0px #00000014;
+  margin: 24px auto;
+  padding: 16px 24px;
+  width: 100%;
+`
+
+export const Title = styled.h2`
+  font-size: 18px;
+  font-weight: 500;
+`
