7c010b659f2d994538c8303c06cd1120bd118f9f
Gabriel Santos <mfkrgabriel@gmail.com>
2022-10-16 16:52:17 -0300

    Add dynamic to the tabs

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,9 +4,16 @@ import Layout from 'components/Layout'
 import Information from 'components/Information'
 import Tabs from 'components/Tabs'
 import SearchWrapper from 'components/SearchWrapper'
+import UnderConstruction from 'components/UnderConstruction'
 
 export default function App() {
   const [actualStep, setActualStep] = useState(1)
+
+  const tabPanel: { [key: number]: () => JSX.Element } = {
+    1: SearchWrapper,
+    2: UnderConstruction,
+    3: UnderConstruction
+  }
 
   const handleStep = (newStep: number) => {
     if (newStep === actualStep) return
@@ -14,11 +21,17 @@ export default function App() {
     setActualStep(newStep)
   }
 
+  const renderCurrentTab = (step: number) => {
+    const TabPanel = tabPanel[step]
+
+    return <TabPanel />
+  }
+
   return (
     <Layout>
       <Information />
       <Tabs actualStep={actualStep} handleStep={handleStep} />
-      <SearchWrapper />
+      {renderCurrentTab(actualStep)}
     </Layout>
   )
 }
